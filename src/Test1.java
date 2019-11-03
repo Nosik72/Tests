973370c568caa0bb43637ff1653fc9dd805ea2e5
Alex Nosik <nosik.av.sd@gmail.com>
2019-11-03 17:33:30 +0300

    Добавлем вывод 333333 в Test1

--- src/Test1.java
+++ src/Test1.java
@@ -2,6 +2,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("111111");
+        System.out.println("333333");
 
     }
 }
