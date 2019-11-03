38ea421ee136193e1ad843597ecb3bfa54dcce03
Alex Nosik <nosik.av.sd@gmail.com>
2019-11-03 17:53:26 +0300

    Добавлем вывод Br в Test1

--- src/Test1.java
+++ src/Test1.java
@@ -3,6 +3,8 @@ public class Test1 {
         System.out.println("Test1");
         System.out.println("111111");
         System.out.println("333333");
+        System.out.println("Ma");
+
 
     }
 }
